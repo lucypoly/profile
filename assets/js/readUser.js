5b6417d49a6439602d03690e2d9a4adca6f87317
pc <Polianychko_Liudmyla@epam.com>
2017-03-15 22:06:12 +0200

    side bar added

--- assets/js/readUser.js
+++ assets/js/readUser.js
@@ -2,9 +2,8 @@ function readData(field) {
     var profileDatabase = firebase.database();
     var user = firebase.auth().currentUser;
     var name = user.email.substring(0, user.email.indexOf("@"));
-    var dataPromise = profileDatabase.ref('/users/' + name + 'id').once('value').then(function (snapshot) {
-        return snapshot.val()[field];
+    return profileDatabase.ref('/users/' + name + 'id').once('value').then(function (snapshot) {
+        console.log(snapshot.val()[field]);
     });
-    return dataPromise();
 }
 
